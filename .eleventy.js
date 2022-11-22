dcccd6843a370cc3c2473edc747ba8403503fe19
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2022-11-22 13:29:12 +0100

    Changed it to the same as the best one

--- .eleventy.js
+++ .eleventy.js
@@ -1,52 +1,41 @@
 const Image = require("@11ty/eleventy-img");
 
 async function imageShortcode(src, alt, sizes) {
-    let metadata = await Image(src, {
-        widths: [300, 600],
-        formats: ['avif', 'jpeg'],
-        outputDir: './dist/img/',
-    });
+  let metadata = await Image(src, {
+    widths: [300, 600, null],
+    outputDir: "./dist/img/",
+  });
 
-    let imageAttributes = {
-        alt,
-        sizes,
-        loading: 'lazy',
-        decoding: 'async',
-    };
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
 
-    // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-    return Image.generateHTML(metadata, imageAttributes);
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
 }
 
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPlugin(lazyImages, {})
-    function lazyImages (eleventyConfig, userOptions = {}) {
-    const {parse} = require('node-html-parser')
-  
-    const options = {
-      name: 'lazy-images',
-      ...userOptions
-    }
-  
-    eleventyConfig.addTransform(options.extensions, (content, outputPath) => {
-      if (outputPath.endsWith('.html')) {
-        const root = parse(content);
-        const images = root.querySelectorAll('img');
-        images.forEach((img) => {
-          img.setAttribute('loading', 'lazy')
-        })
-        return root.toString()
-      }
-      return content;
-    })
-  }
-    eleventyConfig.addPassthroughCopy("src/css");
+
+module.exports = function(eleventyConfig) {
+   // eleventyConfig.addPassthroughCopy("src/css");
+   
+    eleventyConfig.addPassthroughCopy("src/img");//
     eleventyConfig.addPassthroughCopy("src/font");//
-    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+    
+    //     eleventyConfig.addShortcode('image', function (src, alt, width, height, classlist){
+    //     return `<img src="${src}" alt="${alt}" width="${width}" height="${height}">`;
+    //  });
+
+   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+   eleventyConfig.addLiquidShortcode("image", imageShortcode);
+   eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+    
     return {
         dir: {
-            input: 'src',
-            output: 'dist',
-        },
-    };
+            input: "src",
+            output: "dist"
+        }
+    }
 };
